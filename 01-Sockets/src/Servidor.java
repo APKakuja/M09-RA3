94a5e5c680079b1248bba894e5c9bb42ef4affc3
APKakuja <adrianpetrisorkakuja@gmail.com>
2026-04-24 17:07:22 +0200

    01-Sockets fet a classe

---
+++ 01-Sockets/src/Servidor.java
@@ -0,0 +1,10 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+    
+    public static final int PORT = 7777;
+    public static final String HOST = "localhost";
+    private ServerSocket srvSocket;
+    private Socket clientSocket;
+}
